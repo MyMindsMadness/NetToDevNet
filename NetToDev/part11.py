5baccd7c45cece996b785c61e1f995397b3d2b3a
Ian Tony Birchall <iantonybirchall@gmail.com>
2024-08-01 20:32:07 +0100

    NetToDev update

---
+++ NetToDev/part11.py
@@ -0,0 +1,45 @@
+# MyMindsMadness
+# Save config for 
+# change log
+
+from scrapli.driver.core import IOSXEDriver
+import datetime
+
+start = str(datetime.datetime.now())
+dev8k = {
+    'host':'10.10.20.48',
+    'auth_username':'developer',
+    'auth_password':'C1sco12345',
+    'auth_strict_key':False
+}
+
+conn = IOSXEDriver(**dev8k)
+conn.open()
+response = conn.send_command("show ip int br")
+print (response)
+print (response.result)
+
+int_config = []
+intf = "interface vlan"
+vlan = 10
+ip = " ip address"
+ms = "255.255.255.0"
+a = 192
+b = 168
+for c in range(10 , 81 , 10):
+    d = 5
+    int_config.append(f"{intf} {vlan}")
+    int_config.append(f"{ip} {a}.{b}.{c}.{d} {ms}")
+    vlan += 10
+
+print (int_config)
+sendconf = conn.send_commands(int_config)
+response2 = conn.send_command("show ip int br")
+print (response2.result)
+
+end = str(datetime.datetime.now())
+with open ("change.log", "w") as f:
+    f.write(start)
+    f.write(sendconf)
+    f.write(response2)
+    f.write(end)
